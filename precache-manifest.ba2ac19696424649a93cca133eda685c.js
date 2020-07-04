6de19cc5886b045184abd559bc4bfc1d19dce549
Nexhan <subodhkhadka3@gmail.com>
2020-07-03 20:11:56 -0700

    Updates

---
+++ precache-manifest.ba2ac19696424649a93cca133eda685c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff43d9dbd700d1e7b849f087421f6183",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "af3c14e27704962f1d67",
+    "url": "/Pokedex/static/css/main.eceebbf9.chunk.css"
+  },
+  {
+    "revision": "5f3d5050c79beac3930b",
+    "url": "/Pokedex/static/js/2.61d590b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.61d590b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3c14e27704962f1d67",
+    "url": "/Pokedex/static/js/main.83fea72f.chunk.js"
+  },
+  {
+    "revision": "1a3d12fc8495bd59331b",
+    "url": "/Pokedex/static/js/runtime-main.979b9bb2.js"
+  }
+]);
